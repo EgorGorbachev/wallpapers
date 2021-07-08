3e1ce88be6ae1b72f5432095058e62a7616b8400
Egor Gorbachev <e.gorbachev.vironit@vironit.ru>
2021-07-08 15:23:27 +0300

    feat: add wallpaper installation function

--- app/src/main/java/com/example/gorbachev_wallpapers/presentation/fragments/DetailsImageFragment.kt
+++ app/src/main/java/com/example/gorbachev_wallpapers/presentation/fragments/DetailsImageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gorbachev_wallpapers.presentation.fragments
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,13 +11,15 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.navigation.Navigation
@@ -29,9 +32,9 @@ import com.bumptech.glide.request.target.Target
 import com.example.gorbachev_wallpapers.R
 import com.example.gorbachev_wallpapers.databinding.FragmentDetailsImageBinding
 import com.example.gorbachev_wallpapers.presentation.base.BaseFragment
+import com.example.gorbachev_wallpapers.presentation.util.TuneSheetDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_details_image.*
-import retrofit2.http.Url
 import java.io.ByteArrayOutputStream
 
 
@@ -131,11 +134,11 @@ class DetailsImageFragment : BaseFragment(R.layout.fragment_details_image) {
 							return@OnNavigationItemSelectedListener true
 						}
 						R.id.details_menu_settings -> {
-							// put your code here
+							showTuneMenu()
 							return@OnNavigationItemSelectedListener true
 						}
 						R.id.details_menu_info -> {
-							// put your code here
+							showInfo()
 							return@OnNavigationItemSelectedListener true
 						}
 					}
@@ -143,7 +146,31 @@ class DetailsImageFragment : BaseFragment(R.layout.fragment_details_image) {
 				}
 			imageDetailsMenu.setOnNavigationItemSelectedListener(onClickBottomNavItem)
 		}
+		
 	}
+	
+	
+	private fun showTuneMenu(){
+		val tuneMenu = TuneSheetDialog(getBitmapFromView(imageDetailsIV))
+		tuneMenu.show(requireFragmentManager(),"lol")
+	}
+	
+	private fun showInfo(){
+		val inflater: LayoutInflater =
+			binding.root.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+		val popupView = inflater.inflate(R.layout.info_menu, null)
+		
+		val popupWidth = LinearLayout.LayoutParams.MATCH_PARENT
+		val popupHeight = LinearLayout.LayoutParams.WRAP_CONTENT
+		val popupWindow = PopupWindow(popupView, popupWidth, popupHeight)
+		popupWindow.update(0,0, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+		popupWindow.animationStyle = R.style.popup_anim
+		popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0)
+		binding.imageDetailsFullScreen.setOnClickListener {
+			popupWindow.dismiss()
+		}
+	}
+	
 	
 	private fun getBitmapFromView(photo: ImageView): Bitmap? {
 		val bitmap = Bitmap.createBitmap(photo.width, photo.height, Bitmap.Config.ARGB_8888)
@@ -152,7 +179,7 @@ class DetailsImageFragment : BaseFragment(R.layout.fragment_details_image) {
 		return bitmap
 	}
 	
-	private fun getImageUrl(context: Context, photo: Bitmap): Uri? {
+	private fun getImageUri(context: Context, photo: Bitmap): Uri? {
 		
 		val bytes = ByteArrayOutputStream()
 		photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
@@ -161,62 +188,64 @@ class DetailsImageFragment : BaseFragment(R.layout.fragment_details_image) {
 		return Uri.parse(path)
 	}
 	
-	private fun sharePhoto(){
+	private fun sharePhoto() {
 		val photo = getBitmapFromView(imageDetailsIV)
 		val share = Intent(Intent.ACTION_SEND)
 		share.type = "image/*"
-		share.putExtra(Intent.EXTRA_STREAM, getImageUrl(requireContext(),photo!!))
+		share.putExtra(Intent.EXTRA_STREAM, getImageUri(requireContext(), photo!!))
 		startActivity(Intent.createChooser(share, "Share"))
 	}
 	
 	// request permission //////////////////////////////////
 	
 	private fun startRequest() {
-		checkForPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, "camera", WRITE_EXTERNAL_STORAGE_PERMISSION_CODE)
+		checkForPermissions(
+			Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
+		)
 	}
 	
-	private fun checkForPermissions(permission: String, name: String, requestCode: Int) {
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+	private fun checkForPermissions(permission: String, requestCode: Int) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 			when {
-				ContextCompat.checkSelfPermission(requireActivity(), permission) == PackageManager.PERMISSION_GRANTED -> {
+				ContextCompat.checkSelfPermission(
+					requireActivity(),
+					permission
+				) == PackageManager.PERMISSION_GRANTED -> {
 					sharePhoto()
 				}
-				shouldShowRequestPermissionRationale(permission) -> showDialog(permission,name, requestCode)
-				
-				else -> ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission),requestCode)
+				shouldShowRequestPermissionRationale(permission) -> {
+					requestPermissions(
+						arrayOf(
+							Manifest.permission.WRITE_EXTERNAL_STORAGE
+						), requestCode
+					)
+				}
+				else -> requestPermissions(
+					arrayOf(
+						Manifest.permission.WRITE_EXTERNAL_STORAGE
+					), requestCode
+				)
 			}
 		}
 	}
 	
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
 		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 		fun innerCheck(name: String) {
-			if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-				Toast.makeText(requireActivity(),"$name permission refused", Toast.LENGTH_SHORT).show()
+			if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+				Toast.makeText(requireActivity(), "$name permission refused", Toast.LENGTH_SHORT)
+					.show()
 			} else {
-				Toast.makeText(requireActivity(),"$name permission granted", Toast.LENGTH_SHORT).show()
+				toast("$name permission granted")
 				sharePhoto()
 			}
 		}
-		innerCheck("camera")
+		innerCheck("storage")
 	}
-	
-	
-	private fun showDialog(permission: String, name:String, requestCode: Int){
-		
-		val bulder = AlertDialog.Builder(requireContext())
-		
-		bulder.apply {
-			setMessage("Permission for access your $name")
-			setTitle("Permission")
-			setPositiveButton("Ok"){ _, _ ->
-				ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission),requestCode)
-			}
-		}
-		val dialog = bulder.create()
-		dialog.show()
-	}
-	
-	
 	
 }
